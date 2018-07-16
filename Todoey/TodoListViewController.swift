8743ca3627a816c8b1e0e1452dbea7f0aaa9ce29
Connor <Connor@connors-imac>
2018-07-16 09:05:36 -0400

    Add new item functionality complete.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Connor on 7/14/18.
+//  Copyright © 2018 Connor. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+ 
+    //Want the three items in the itemArray to be listed in three separate cells
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    
+    }
+    //MARK -TableView Delegate Method that gets fired whenever you select something from the table view
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        //prints number into debug console
+        
+        //everytime you select the cell it will add a checkmark
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        //animates the deselction once the item has been tapped. 
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
+    
+    //Mark - Add New Items (uses a press on the bar button item to add a new item to the todo list)
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        var textField = UITextField()
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the add item button on UIAlert. We want to append the item array to include whatever is added to the add item button. Because we are inside a close (see the "in" above) we have to specify self to specify that the item array exists within the current class
+            self.itemArray.append(textField.text!)
+        
+            //need to reload the data for the itemArray to take into account the newly added item
+            self.tableView.reloadData()
+        
+        }
+        
+        
+        
+        //want to add text field to the alert so that we can add in the new item instead of just having it display "New Item
+        alert.addTextField{ (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+}
+
